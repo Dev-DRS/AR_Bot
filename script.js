368d0c221a1ab75ba8079e73d2feb3feee577006
Nimit Bhinde <nimit19bhinde98@gmail.com>
2024-08-23 19:07:56 +0530

    Last Commit by Nimit

--- script.js
+++ script.js
@@ -11,3 +11,44 @@ const onProgress = (event) => {
   }
 };
 document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
+// Migrated Code //
+document.querySelectorAll('.next-button').forEach(button => {
+  button.addEventListener('click', function () {
+      const currentModal = this.closest('.modal');
+      const nextModalId = this.getAttribute('data-next');
+      if (nextModalId === 'done') {
+          currentModal.style.display = 'none';
+      } else {
+          const nextModal = document.getElementById(nextModalId);
+          currentModal.classList.add('modal-slide-out-left');
+          currentModal.addEventListener('animationend', () => {
+              currentModal.style.display = 'none';
+              currentModal.classList.remove('modal-slide-out-left');
+          });
+          nextModal.style.display = 'block';
+          nextModal.classList.add('modal-slide-in-right');
+          nextModal.addEventListener('animationend', () => {
+              nextModal.classList.remove('modal-slide-in-right');
+          });
+      }
+  });
+});
+
+// Function to show only the settings icon
+const settingIcon = document.getElementById('setting-icon');
+
+document.getElementById('start-button').addEventListener('click', function() {
+    document.getElementById('Help').style.display = 'none';
+    document.getElementById('responseText-area').style.display = 'block';
+    document.getElementById('recBtn').style.display = 'block';
+    
+    document.getElementById('menu-open').style.display = 'none';
+    //$('#startModal').modal('hide');
+    // document.getElementById('menu-open').style.display = 'none';
+    // settingIcon.style.display = 'block';
+    document.getElementById('startModal').style.display = 'none';
+    
+});
+
+// Migrated Code //
